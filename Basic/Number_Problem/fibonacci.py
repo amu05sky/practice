3e7626d4bc72e513085303f3644625958707604d
Shaik_Amreen <amu05sky@gmail.com>
2023-11-14 19:07:45 +0000

    practice

--- Basic/Number_Problem/fibonacci.py
+++ Basic/Number_Problem/fibonacci.py
@@ -1,11 +1,10 @@
 #Fibonacci Series
-
-n = int(input())
+n = int(input("Limit: "))
 a = 0
 b = 1
 
 for i in range(n):
-    print(a, end=" ")
+    print(a, end="\n")
     c = a + b
     a = b
     b = c
